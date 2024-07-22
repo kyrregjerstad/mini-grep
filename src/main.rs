b6e007d3f400d78b77eeb4aed39d65c2071e85d1
Kyrre Gjerstad <kyrregjerstad@gmail.com>
2024-07-22 10:21:42 +0200

    update to use iterators

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,8 @@ use mini_grep::Config;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::build(&args).unwrap_or_else(|err| {
-        eprintln!("Problem parsing arguments: {err}");
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
+        eprint!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
